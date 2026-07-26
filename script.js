e049c50bf20942b66570a8cd9e6716ed3ac7fcc8
mdshaibaazkhan2010-creator <mdshaibaazkhan2010@gmail.com>
2026-07-26 18:57:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,90 @@
-// Physics Very Easy
+// ==============================
+// PHYSICS VERY EASY V2
+// ==============================
 
-console.log("Physics Very Easy Loaded!");
-
-function welcome() {
-    alert("Welcome to Physics Very Easy! 🚀");
-}
-
-function comingSoon() {
-    alert("🚧 This feature is coming soon!");
-}
-
-// Run when website loads
+// Welcome
 window.onload = function () {
-    console.log("Website Ready!");
+    console.log("Physics Very Easy Loaded 🚀");
 };
+
+// Dark Mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+// Search Chapters
+function searchChapter() {
+
+    let input = document.getElementById("searchBox").value.toLowerCase();
+
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+
+        if(card.innerText.toLowerCase().includes(input))
+            card.style.display="block";
+        else
+            card.style.display="none";
+
+    });
+
+}
+
+// Smooth Scroll Button
+function scrollTopPage(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+}
+
+// Counter Animation
+
+const counters=document.querySelectorAll(".stat-box h2");
+
+counters.forEach(counter=>{
+
+counter.innerText="0";
+
+const update=()=>{
+
+const target=counter.getAttribute("data-target");
+
+const c=+counter.innerText;
+
+const increment=target/100;
+
+if(c<target){
+
+counter.innerText=Math.ceil(c+increment);
+
+setTimeout(update,20);
+
+}
+
+else{
+
+counter.innerText=target;
+
+}
+
+};
+
+if(counter.hasAttribute("data-target"))
+
+update();
+
+});
+
+// Coming Soon Alert
+
+function comingSoon(){
+
+alert("🚀 This chapter will be added soon!");
+
+}
