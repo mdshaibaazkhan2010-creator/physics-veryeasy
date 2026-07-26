5a95f766f6043dd93dacc06ff9c9a06ec091c21d
mdshaibaazkhan2010-creator <mdshaibaazkhan2010@gmail.com>
2026-07-26 18:37:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Physics Very Easy website loaded!");
+// Physics Very Easy
+
+console.log("Physics Very Easy Loaded!");
+
+function welcome() {
+    alert("Welcome to Physics Very Easy! 🚀");
+}
+
+function comingSoon() {
+    alert("🚧 This feature is coming soon!");
+}
+
+// Run when website loads
+window.onload = function () {
+    console.log("Website Ready!");
+};
